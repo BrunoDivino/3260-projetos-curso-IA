b473caa44c04d39f2dead7677f271a081ca9909b
Bruno Divino <brunodivino.dev@gmail.com>
2023-07-21 15:40:41 -0300

    Projeto 2

--- sumarizador.py
+++ sumarizador.py
@@ -7,27 +7,29 @@ load_dotenv()
 openai.api_key = os.getenv('OPEN_AI_API_KEY')
 
 system_msg = '''
-Você é um extrator de informações. Você recebe um anúncio de marketing
-sobre um produto e retorna os dados brutos sobre o produto em formato JSON. 
-Exemplo de retorno:
-{
-  "marca": "Pear",
-  "modelo": "Notebook Pear",
-  "cor": "Preto"
-}
+Você é um sumarizador de textos. Você vai receber um feedback de um
+consumidor a respeito de um produto e definir qual o perfil desse
+consumidor. 
+
+O formato de retorno deverá ser um texto curto resumindo
+três pontos: Nível de satisfação do consumidor; probabilidade de 
+realizar mais compras; Tipos de produtos que podem interessar o 
+consumidor.
 '''
 
-message = input("Informe o anúncio: ")
+message = input("Informe o texto: ")
 
 try:
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
+        temperature=1,
+        max_tokens=500,
         messages=[
             {"role": "system", "content": system_msg},
             {"role": "user", "content": message},
         ]
     )
     reply = response["choices"][0]["message"]["content"]
-    print("\nResposta em JSON:\n" + reply + "\n")
+    print("\nPerfil do consumidor:\n" + reply + "\n")
 except Exception as e:
     print("Ocorreu um erro na solicitação à API: ", e)
